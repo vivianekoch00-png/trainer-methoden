51dd06bbeaed5f3a3aebabc4c2c117f62a138c79
vivianekoch00-png <viviane.koch00@gmail.com>
2026-04-12 12:40:45 +0200

    Fix: remove unused React imports (TS6133)

--- src/pages/Methods.tsx
+++ src/pages/Methods.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { fetchMethods } from "../lib/api";
 import type { Method } from "../lib/api";
