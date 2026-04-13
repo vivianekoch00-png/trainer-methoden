de7c1dc2f24f15da59cd8e50cdfac8ebaf881dab
vivianekoch00-png <viviane.koch00@gmail.com>
2026-04-13 14:52:58 +0200

    Add Admin page

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Admin-Bereich</h1>
+      <p>Hier werden später Methoden gepflegt.</p>
+    </div>
+  );
+}
